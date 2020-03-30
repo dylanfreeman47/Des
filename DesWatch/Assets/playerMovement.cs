28f259e22dca66788196c6f22635a97f111c8da2
dylanfreeman47 <dylanfreeman47@yahoo.com>
2020-03-30 00:37:08 -0400

    Add a character, temp asset, and attempt movement

---
+++ DesWatch/Assets/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        // Movement
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
